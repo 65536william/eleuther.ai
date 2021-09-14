48f4aa6dc00aad98ea3e675c16405020b8eef80f
William <admin@williameliot.com>
2021-09-14 08:31:57 +0100

    feat: add first blog post and blog grid

--- content/blog/rotary-embeddings.md
+++ content/blog/rotary-embeddings.md
@@ -1,7 +1,19 @@
 ---
 title: "Rotary Embeddings: A Relative Revolution"
-date: 2020-04-20
-thumbnail: /rotary-embeddings.jpg
+publicationDate: 2021-04-20T11:00:09.843Z
+authors:
+  - Stella Biderman
+  - Sid Black
+  - Charles Foster
+  - Leo Gao
+  - Eric Hallahan
+  - Horace He
+  - Ben Wang
+  - Phil Wang
+coverImage: public/images/janus.png
+lead: Rotary Positional Embedding (RoPE) is a new type of position encoding that
+  unifies absolute and relative approaches. We put it to the test.
 ---
+## TL:DR;
 
-Rotary Positional Embedding (RoPE) is a new type of position encoding that unifies absolute and relative approaches. We put it to the test.
+Rotary Positional Embedding (RoPE) is a new type of position encoding that unifies absolute and relative approaches. Developed by Jianlin Su in a series of blog posts earlier this year \[12, 13] and in a new preprint \[14], it has already garnered widespread interest in some Chinese NLP circles. This post walks through the method as we understand it, with the goal of bringing it to the attention of the wider academic community. In general we have found that across a large suite of setups including regular, linear, and local self-attention, **it either matches or surpasses all other methods currently available for injecting positional information into transformers.**
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,6 @@
+import Layout from "../../components/Layout";
+import Image from "next/image";
+
 const importBlogPosts = async () => {
   const markdownFiles = require
     .context("../../content/blog", false, /\.md$/)
@@ -23,9 +26,50 @@ export async function getStaticProps() {
 }
 
 export default function BlogIndex({ postsList }) {
+  console.log(postsList);
   return (
-    <div>
-      <h2>Blog</h2>
-    </div>
+    <Layout>
+      <div className="blogIndex">
+        <div>
+          <h2>Blog</h2>
+        </div>
+        <div className="blogPosts">
+          {postsList.map((blogPost) => (
+            <div className="postCard">
+              {blogPost.attributes.coverImage && (
+                <Image
+                  src={blogPost.attributes.coverImage.substring(
+                    blogPost.attributes.coverImage.indexOf("/")
+                  )}
+                  layout="responsive"
+                  width="200px"
+                  height="100px"
+                />
+              )}
+              <div className="cardMeta">
+                <h3>{blogPost.attributes.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <style jsx>{`
+        .blogIndex {
+          margin: 0 10vw;
+        }
+        .blogPosts {
+          display: grid;
+          grid-template-columns: repeat(4, 20vw);
+          gap: 10%;
+        }
+        .postCard {
+          border: 2px solid black;
+          border-radius: 0.25rem;
+        }
+        .cardMeta {
+          padding: 1.25rem;
+        }
+      `}</style>
+    </Layout>
   );
 }
